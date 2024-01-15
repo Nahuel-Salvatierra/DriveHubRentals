7642af77f1a7c50c48719898d635b2be9aed1a02
Nahuel <salvatierra.nahuel@gmai.com>
2024-01-15 16:23:32 -0300

    Update service tests

--- src/module/customer/app/service/__test__/customer.service.spec.ts
+++ src/module/customer/app/service/__test__/customer.service.spec.ts
@@ -2,19 +2,33 @@ import { CustomerRepository } from "../../../customer.module";
 import { customerModel } from "../../repository/__test__/customer.repository.spec";
 import { CustomerService } from "../customer.service";
 
-const repositoryMock: CustomerRepository = {
-	saveCustomer: jest.fn(),
-	getCustomerById: jest.fn(),
-	getAllCustomers: jest.fn(),
-	deleteCustomer: jest.fn(),
-	customerModel: customerModel,
-};
+describe("Customer Service", () => {
+	const repositoryMock: CustomerRepository = {
+		save: jest.fn(),
+		getById: jest.fn(),
+		getAll: jest.fn(),
+		delete: jest.fn(),
+		customerModel: customerModel,
+	};
 
-const service = new CustomerService(repositoryMock);
+	const customerService = new CustomerService(repositoryMock);
+	it("Should call save once", async () => {
+		customerService.create({});
+		expect(repositoryMock.save).toHaveBeenCalledTimes(1);
+	});
 
-describe("Customer service", () => {
-	it.only("Should call saveCustomer once", async () => {
-    service.create({});
-		expect(repositoryMock.saveCustomer).toHaveBeenCalledTimes(1);
+	it("Should call getById once", async () => {
+		customerService.getById(1);
+		expect(repositoryMock.getById).toHaveBeenCalledTimes(1);
+	});
+
+	it("Should call getAll once", async () => {
+		customerService.getAll();
+		expect(repositoryMock.getAll).toHaveBeenCalledTimes(1);
+	});
+
+	it("Should call delete once", async () => {
+		customerService.delete(1);
+		expect(repositoryMock.delete).toHaveBeenCalledTimes(1);
 	});
 });
