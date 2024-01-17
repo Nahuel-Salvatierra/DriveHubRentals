e0cb400d2eb8510c5014065b193fa3f5601b9261
Nahuel <salvatierra.nahuel@gmai.com>
2024-01-17 14:55:33 -0300

    Add car mapper tests

---
+++ src/module/customer/application/mapper/__test__/customer.mapper.spec.ts
@@ -0,0 +1,28 @@
+import Customer from "../../../domain/customer.entity";
+import { fromCustomerModelToEntity } from "../fromCustomerModelToEntity";
+import { sampleCustomer } from "../../../../../__test__/customer.fixture";
+
+describe("Mappers", () => {
+	it("Should convert a customer dto to entity", () => {
+		const customerDto = fromCustomerModelToEntity(sampleCustomer);
+		expect(customerDto).toBeInstanceOf(Customer);
+		expect(customerDto.address).toBeDefined()
+		expect(customerDto.email).toBeDefined()
+		expect(customerDto.phone).toBeDefined()
+		expect(customerDto.birthDate).toBeDefined()
+		expect(customerDto.documentNumber).toBeDefined()
+		expect(customerDto.documentType).toBeDefined()
+		expect(customerDto.lastName).toBeDefined()
+		expect(customerDto.name).toBeDefined()
+	});
+
+	it("Should convert a customer model to entity", () => {
+		const customerModel = fromCustomerModelToEntity({
+			sampleCustomer,
+			id: 2,
+			createAt: new Date(),
+			updateAt: new Date(),
+		});
+    expect(customerModel).toBeInstanceOf(Customer)
+	});
+});
