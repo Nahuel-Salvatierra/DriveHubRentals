dade7233bd81f0707ccda6b79c38540e9e18dc82
Nahuel <salvatierra.nahuel@gmai.com>
2024-01-18 10:14:06 -0300

    Add rent entity

---
+++ src/module/rent/domain/rent.entity.ts
@@ -0,0 +1,13 @@
+export class Rent {
+	id?: number;
+	carId?: number;
+	unitPrice?: number;
+	totalPrice?: number;
+	paymentMethod?: string;
+	isPaid?: boolean;
+	customerId?: number;
+	startDate?: Date;
+	endDate?: Date;
+	createdAt?: Date;
+	updatedAt?: Date;
+}
