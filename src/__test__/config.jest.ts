e63487cad41cd32721c7270c260c96e30126f071
Nahuel <salvatierra.nahuel@gmai.com>
2024-01-18 15:57:03 -0300

    Update name file

--- src/__test__/config.jest.ts
+++ src/__test__/config.jest.ts
@@ -1,9 +1,11 @@
 import { CarModel } from "../module/car/car.module";
 import { CustomerModel } from "../module/customer/customer.module";
 import { Sequelize } from "sequelize-typescript";
+import { RentModel } from "../module/rent/rent.module";
 
 export const sequelizeInstance = new Sequelize("sqlite::memory:", {
 	logging: false,
 });
 export const customerModel = CustomerModel.setup(sequelizeInstance);
 export const carModel = CarModel.setup(sequelizeInstance)
+export const rentModel = RentModel.setup(sequelizeInstance)
