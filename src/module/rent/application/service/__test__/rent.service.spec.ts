ac87c5a3ae29336d9a6dfbd593616735cd86ae85
Nahuel <salvatierra.nahuel@gmai.com>
2024-01-23 15:05:01 -0300

    Refactor tests and add more

--- src/module/rent/application/service/__test__/rent.service.spec.ts
+++ src/module/rent/application/service/__test__/rent.service.spec.ts
@@ -3,8 +3,12 @@ import {
 	customerServiceMock,
 	rentRepositoryMock,
 } from "../../../../../__test__/__mocks__/mocks";
+import {
+	sequelizeInstance,
+} from "../../../../../__test__/config.jest";
 import { sampleRent } from "../../../../../__test__/rent.fixture";
 import { Rent } from "../../../domain/rent.entity";
+import { UpdateRentDto } from "../../dto/update.rent.dto";
 import { RentService } from "../rent.service";
 
 describe("Rent Service", () => {
@@ -15,99 +19,50 @@ describe("Rent Service", () => {
 			carServiceMock,
 			customerServiceMock
 		);
+		sequelizeInstance.sync({ force: true });
 	});
+
+	afterEach(()=>{
+		jest.clearAllMocks();
+	})
 
 	it("Should call save once", async () => {
 		const totalPrice = jest.spyOn(rentService, "setTotalPrice");
-		const validate = jest.spyOn(rentService, "validateTransaction");
 		await rentService.create(sampleRent);
 		expect(rentRepositoryMock.save).toHaveBeenCalledTimes(1);
-		expect(rentRepositoryMock.save).toHaveBeenCalledWith(sampleRent);
 		expect(totalPrice).toHaveBeenCalled();
-		expect(validate).toHaveBeenCalled();
-	});
-
-	it("Should validate transaction", async () => {
-		const checkCar = jest.spyOn(rentService, "checkCarRent")
-		const checkCustomer = jest.spyOn(rentService, "checkCustomerRent")
-		await rentService.validateTransaction(
-			sampleRent.customerId,
-			sampleRent.carId
-		);
-		expect(carServiceMock.getById).toHaveBeenCalledWith(1);
-		expect(customerServiceMock.getById).toHaveBeenCalledWith(1);
-		expect(checkCar).toHaveBeenCalled();
-		expect(checkCustomer).toHaveBeenCalled();
 	});
 
 	it("Should validate if car has a rented", async () => {
-		await rentService.checkCarRent(1);
+		jest.spyOn(rentRepositoryMock, "findByCarId").mockResolvedValue({
+			id: 1,
+			status: "pending",
+		} as Rent )
+		jest.spyOn(rentRepositoryMock, "findByCustomerId").mockResolvedValue(null)
+		try {
+			await rentService.create(sampleRent);
+		} catch (error) {
+			expect(error).toBeDefined()
+		}
 		expect(rentRepositoryMock.findByCarId).toHaveBeenCalled();
 		expect(rentRepositoryMock.findByCarId).toHaveBeenCalledWith(1);
 	});
 
 	it("Should validate if customer has a rented", async () => {
-		await rentService.checkCustomerRent(1);
+		jest.spyOn(rentRepositoryMock, "findByCustomerId").mockResolvedValue({
+			id: 1,
+			status: "pending",
+		} as Rent)
+		jest.spyOn(rentRepositoryMock, "findByCarId").mockResolvedValue(null)
+		try {
+			await rentService.create(sampleRent)
+		} catch (error) {
+			expect(error).toBeDefined()
+		}
 		expect(rentRepositoryMock.findByCustomerId).toHaveBeenCalled();
 		expect(rentRepositoryMock.findByCustomerId).toHaveBeenCalledWith(1);
 	});
 
-	it("Should throw error car in rent has status pending", async () => {
-		jest.spyOn(rentRepositoryMock, "findByCarId").mockResolvedValue({
-			id: 1,
-			status: "pending",
-		} as Rent);
-		try {
-			await rentService.checkCarRent(1);
-		} catch (error) {
-			expect(error).toBeDefined();
-		}
-	});
-
-	it("Should throw error customer in rent has status pending", async () => {
-		jest.spyOn(rentRepositoryMock, "findByCustomerId").mockResolvedValue({
-			id: 1,
-			status: "pending",
-		} as Rent);
-		try {
-			await rentService.checkCarRent(1);
-		} catch (error) {
-			expect(error).toBeDefined();
-		}
-	});
-
-	it("Should throw error if car is already rented", async () => {
-		jest.spyOn(rentRepositoryMock, "findByCarId").mockResolvedValue({
-			id: 1,
-		} as Rent);
-		try {
-			await rentService.checkCarRent(1);
-		} catch (error) {
-			expect(error).toBeDefined();
-		}
-	});
-
-	it("Should throw error if customer has a rented", async () => {
-		jest.spyOn(rentRepositoryMock, "findByCustomerId").mockResolvedValue({
-			id: 1,
-		} as Rent);
-		try {
-			await rentService.checkCustomerRent(1);
-		} catch (error) {
-			expect(error).toBeDefined();
-		}
-	});
-
-	it("Should be false when car is available", async () => {
-	 const result = await rentService.checkCarRent(99);
-	 expect(result).toBe(false);
-	})
-	
-	it("Should be false when customer is available", async () => {
-		const result = await rentService.checkCustomerRent(99);
-		expect(result).toBe(false);
-	 })
- 
 	it("Should set total price", () => {
 		const totalPrice = rentService.setTotalPrice(
 			new Date("2022-10-20 10:23:42"),
@@ -115,5 +70,27 @@ describe("Rent Service", () => {
 			90
 		);
 		expect(totalPrice).toBe(900);
+	});
+
+	it("Should call getAll once", async () => {
+		await rentService.getAll();
+		expect(rentRepositoryMock.getAll).toHaveBeenCalledTimes(1);
+	})
+
+	it("Should call getById once", async () => {
+		await rentService.getById(1);
+		expect(rentRepositoryMock.getById).toHaveBeenCalledTimes(1);
+		expect(rentRepositoryMock.getById).toHaveBeenCalledWith(1);
+	})
+
+	it("Should call delete once", async () => {
+		await rentService.delete(1);
+		expect(rentRepositoryMock.delete).toHaveBeenCalledTimes(1);
+		expect(rentRepositoryMock.delete).toHaveBeenCalledWith(1);
+	})
+
+	it("Should call update once", async () => {
+		await rentService.update(sampleRent as UpdateRentDto, 1);
+		expect(rentRepositoryMock.save).toHaveBeenCalledTimes(1);
 	})
 });
--- src/module/rent/controller/__test__/rent.controller.spec.ts
+++ src/module/rent/controller/__test__/rent.controller.spec.ts
@@ -1,7 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import { rentServiceMock } from "../../../../__test__/__mocks__/mocks";
-import { RentController } from "../../rent.module";
+import { RentController, RentService } from "../../rent.module";
 import { sampleRent } from "../../../../__test__/rent.fixture";
+import { Rent } from "../../domain/rent.entity";
 
 describe("Rent Controller", () => {
 	let controller: RentController;
@@ -15,38 +16,37 @@ describe("Rent Controller", () => {
 	let next: NextFunction = jest.fn();
 
 	beforeAll(() => {
-		controller = new RentController(rentServiceMock);
+		controller = new RentController(rentServiceMock as RentService);
 	});
 	it("Should create a new rent", async () => {
-		await controller.create(
-			req as Request,
-			res as Response,
-			next
-		);
+		await controller.create(req as Request, res as Response, next);
 		expect(rentServiceMock.create).toHaveBeenCalledTimes(1);
-		expect(rentServiceMock.create).toHaveBeenCalledWith(sampleRent)
+		expect(rentServiceMock.create).toHaveBeenCalledWith(sampleRent);
 	});
 
 	it("Should get all rents", async () => {
 		await controller.getAll(req as Request, res as Response, next);
 		expect(rentServiceMock.getAll).toHaveBeenCalledTimes(1);
-	})
+	});
 
 	it("Should get a rent by ID", async () => {
 		await controller.getById(req as Request, res as Response, next);
 		expect(rentServiceMock.getById).toHaveBeenCalledTimes(1);
-		expect(rentServiceMock.getById).toHaveBeenCalledWith(1)
-	})
+		expect(rentServiceMock.getById).toHaveBeenCalledWith(1);
+	});
 	it("Should delete a rent by ID", async () => {
 		await controller.delete(req as Request, res as Response, next);
 		expect(rentServiceMock.delete).toHaveBeenCalledTimes(1);
-		expect(rentServiceMock.delete).toHaveBeenCalledWith(1)
-	})
+		expect(rentServiceMock.delete).toHaveBeenCalledWith(1);
+	});
 
 	it("Should update a rent by ID", async () => {
+		const partialRent: Partial<Rent> = {
+			unitPrice: 200,
+			paymentMethod: "cash",
+		};
+		req.body = partialRent;
 		await controller.update(req as Request, res as Response, next);
 		expect(rentServiceMock.update).toHaveBeenCalledTimes(1);
-		expect(rentServiceMock.update).toHaveBeenCalledWith(sampleRent, 1)
-	})
-
+	});
 });
